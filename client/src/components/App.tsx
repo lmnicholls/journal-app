a7c2ee702bb2607f37bbe97c371e2282193f0c87
Lauren Nicholls <lauren.nicholls1@gmail.com>
2021-10-01 09:56:09 -0400

    updated styled components

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,14 +1,24 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { fetchUser } from "../actions";
 import "../App.css";
 
+interface AuthState {
+  auth: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  authenticated: string;
+}
+
+interface RootState {
+  auth: AuthState;
+}
+
 const App = (props: any) => {
   const dispatch = useDispatch();
-  const authenticated = useSelector<any, any>(
-    (state) => state.auth.authenticated
-  );
+  const { authenticated } = useSelector((state: RootState) => state.auth);
 
   useEffect(() => {
     if (authenticated) {
